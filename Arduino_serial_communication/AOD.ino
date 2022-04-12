983ec636b04d63f7a3fdc19e97ca655dd3e29873
Kevin Varela <98143109+KevinNVT@users.noreply.github.com>
2022-04-12 13:02:47 -0600

    Add files via upload

---
+++ Arduino_serial_communication/AOD.ino
@@ -0,0 +1,32 @@
+const int analogPin0 = A0;
+
+int analogValue0;
+
+unsigned long lastTime,sampleTime;
+
+void setup() {
+
+  Serial.begin(9600);
+
+  analogValue0 = 0;
+
+  sampleTime = 40;
+  lastTime = millis();
+
+}
+
+void loop() {
+
+  if (millis() - lastTime >= sampleTime)
+  {
+    lastTime=millis();
+    analogValue0 = analogRead(analogPin0);
+
+    Serial.println(scaling(analogValue0,0,1023,0,100));
+  }
+}
+
+float scaling(float x, float in_min, float in_max, float out_min, float out_max)
+{
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
---
+++ Data_acquisition.py
@@ -0,0 +1,125 @@
+from pyArduino import * 
+from tkinter import *
+import datetime
+import collections
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import pandas as pd
+import numpy as np
+import threading
+
+isRun = True
+isReceive = False
+TempMin = 100
+TempMax = 0
+
+tiempo = datetime.datetime.now() #Tiempo in spanish means time
+
+ts = 1
+tf = 30
+t = np.arange(0,tf+ts,ts)
+N = len(t)
+
+tempMin = 0
+
+port = 'COM3' #This port is according to where arduino was connected
+baudRate = 9600
+sizeData = 1
+
+if isRun:
+    arduino = serialArduino(port,baudRate,sizeData)
+    arduino.readSerialStart()
+    print(f"   Day           Hour        Temperature")
+
+y = np.zeros((sizeData,N))
+
+def getValue():
+    global isRun
+    global samples
+    time.sleep(0.5)
+    while isRun:
+
+        for k in range(N):
+
+             start_time = time.time()
+
+        for n in range(sizeData):
+    
+            y[0,k] = arduino.rawData[0]
+
+            print(tiempo.strftime('%d/%m/%Y     %H:%M:%S'), f"       {y[0,k]}")
+
+        elapsed_time = time.time() - start_time
+
+        while elapsed_time<ts:
+            elapsed_time = time.time()-start_time
+
+hilo1 = threading.Thread(target=getValue,
+                         daemon=True) #Create a thread before serial communication starts
+
+
+def Iniciar():  #Iniciar = Start
+    global isReceive
+    isReceive = True
+    hilo1.start()
+
+def Parar():    #Parar = Stop
+    global isRun
+    global y
+    isRun=False
+    arduino.close() #Close serial communication
+    hilo1.join(0.1)
+    
+def Min():
+    global TempMin
+    value = arduino.rawData[0]
+    if value < TempMin:
+        TempMin = value
+        lblMin["text"] = TempMin
+
+def Max():
+    global TempMax
+    value = arduino.rawData[0]
+    if value > TempMax:
+        TempMax = value
+        lblMax["text"] = TempMax
+            
+ventana = Tk()
+ventana.title("Lectura temperatura")
+ventana.geometry("400x200") #Window size
+ventana["bg"]="gray77"
+
+
+btnIniciar = Button(ventana, text="Iniciar", command = Iniciar)
+btnIniciar["fg"]="black"
+btnIniciar["bg"]="lime green"
+btnIniciar["font"]="Helvetica 12"
+btnIniciar.place(x=30,y=30,width=100,height=60)
+
+btnParar = Button(ventana, text= "Parar", command = Parar)
+btnParar["fg"]="black"
+btnParar["bg"]="orange red"
+btnParar["font"]="Helvetica 12"
+btnParar.place(x=150,y=30,width=100,height=60)
+
+btnMinimo = Button(ventana, text= "Temperatura Minima", command = Min)
+btnMinimo["fg"]="black"
+btnMinimo["bg"]="SkyBlue3"
+btnMinimo.place(x=30,y=110,width=150,height=30)
+
+btnMaxima = Button(ventana, text= "Temperatura Maxima", command = Max)
+btnMaxima["fg"]="black"
+btnMaxima["bg"]="gold"
+btnMaxima.place(x=30,y=160,width=150,height=30)
+
+lblMin= Label(ventana)
+lblMin["fg"]="black"
+lblMin["bg"]="SkyBlue3"
+lblMin.place(x=200,y= 110, width=150,height=30)
+
+lblMax= Label(ventana)
+lblMax["fg"]="black"
+lblMax["bg"]="gold"
+lblMax.place(x=200,y= 160, width=150,height=30)
+
+ventana.mainloop()
